dc00e0775816fc0ee73ee7d4e3e74e6305804377
aoneban <torgregion1@gmail.com>
2023-10-03 20:17:41 +0200

    Changes to be committed
    modified:   src/app/home/home.component.css
    modified:   src/app/home/home.component.html
    modified:   src/app/home/home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
-// import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-home',
@@ -19,12 +18,11 @@ export class HomeComponent implements OnInit {
     const apiKey = '750447c2-3f08-4a4a-b7ea-2dc529472642';
     const url = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1';
 
-    // Определите заголовки, включая 'X-API-KEY':
     const headers = new HttpHeaders({
       'X-API-KEY': apiKey,
       'Content-Type': 'application/json',
     });
-    // Выполните GET-запрос с HttpClient и сохраните данные в переменную filmData:
+
     this.http.get(url, { headers: headers })
       .subscribe(
         (response) => {
@@ -36,9 +34,7 @@ export class HomeComponent implements OnInit {
       );
   }
 
-  // Метод для перехода к карточке фильма
   openFilmDetails(filmId: number) {
-    // Используем маршрутизацию для перехода к карточке фильма
     this.router.navigate(['/film', filmId]);
   }
 }
